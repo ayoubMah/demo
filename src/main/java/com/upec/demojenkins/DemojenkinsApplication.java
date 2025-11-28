8e69419087d7b276c6e78fa4a9563c0477284245
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-11-28 02:55:43 +0100

    init

---
+++ src/main/java/com/upec/demojenkins/DemojenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.upec.demojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojenkinsApplication.class, args);
+    }
+}
